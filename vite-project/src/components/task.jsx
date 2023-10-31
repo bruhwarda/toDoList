96f758371584ff8f804b6959f19f869c596e867b
warda sheraz <wbywardah@gmail.com>
2023-10-31 14:09:50 +0500

    completed task

--- vite-project/src/components/task.jsx
+++ vite-project/src/components/task.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const Task = () => {
+    const [addTask,setTask]=useState([]);
+    const addNameHandler=()=>{
+       const inputName=document.getElementById("name").value
+       console.log("first inputName",inputName)
+      setTask([...addTask,{task:inputName}])
+       console.log("setter variable",addTask)
+
+
+    }
+    
   return (
     <div>
-        <input type="text" id="text id"/>
-        
+        <input type="text" id="name"/>
+        <button onClick={addNameHandler}>
+            Add Task Name
+        </button>
+       {addTask.map((items,index)=>{   
+        return(
+            <div key={index}>
+            <h3>{items.task}</h3>
+        </div>
+        )
+       })}
     </div>
   )
 }
